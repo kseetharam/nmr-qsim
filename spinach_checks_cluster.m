eccdc78956f0c69ac5bbe24bdbbb63ac70b32245
Kushal Seetharam <kushalseetharam@gmail.com>
2023-03-24 00:42:45 -0400

    deut

--- spinach_checks_cluster.m
+++ spinach_checks_cluster.m
@@ -9,9 +9,9 @@ addpath('generator_data/solids')
 
 %%%%%%% LIQUIDS
 
-[spin_system, parameters, H, R, K] = build_noesy_sucrose();
-save(strcat(datapath,'/generators_noesy_sucrose.mat'), 'H', 'R', 'K', 'spin_system', 'parameters','-v7.3')
+% [spin_system, parameters, H, R, K] = build_noesy_sucrose();
+% save(strcat(datapath,'/generators_noesy_sucrose.mat'), 'H', 'R', 'K', 'spin_system', 'parameters','-v7.3')
 
-% [spin_system, parameters, H, R, K] = build_noesyhsqc_ubiquitin_deut();
-% save(strcat(datapath,'/generators_noesyhsqc_ubiquitin_deut.mat'), 'H', 'R', 'K', 'spin_system', 'parameters','-v7.3')
+[spin_system, parameters, H, R, K] = build_noesyhsqc_ubiquitin_deut();
+save(strcat(datapath,'/generators_noesyhsqc_ubiquitin_deut.mat'), 'H', 'R', 'K', 'spin_system', 'parameters','-v7.3')
 
